806b997d56f7cf7f007e34648e1a9e40d14daca4
Bol4onok <Bol4onok@mail.com>
2022-11-21 02:38:02 +0300

    add slider clothes

--- frontend/src/components/pages/Clothes/ImageBlock.tsx
+++ frontend/src/components/pages/Clothes/ImageBlock.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Slider from "react-slick";
+import { useState } from "react";
 
 import { albumImageType } from "@/@types/pages/Product";
 
 import { productPropertiesSelector } from "@/redux/productProperties/selectors";
 
-import { returnValidColor } from "@/utils/hasColor";
+import { returnValidColor, returnValidColors } from "@/utils/hasColor";
 
 type ImageBlockType = {
   albumImages: albumImageType | undefined;
@@ -14,14 +16,36 @@ type ImageBlockType = {
 export const ImageBlock: React.FC<ImageBlockType> = ({ albumImages, pk }) => {
   const { propertiesList } = useSelector(productPropertiesSelector);
 
+  const [nav1, setNav1] = useState(0);
   if (!albumImages) {
     return <img alt="eccomerce" src="" />;
   }
+  const onClickIndex = (index: number) => {
+    setNav1(index);
+  };
   return (
-    <img
-      alt="ecommerce"
-      className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-      src={returnValidColor(propertiesList, pk, albumImages)}
-    />
+    <div className="h-max p-1 md:w-1/2 max-w-md border bg-white rounded shadow-lg">
+      <img
+        className="object-cover object-center pb-1 h-96 w-max "
+        src={returnValidColors(propertiesList, pk, albumImages)[nav1]}
+      />
+      <Slider
+        cssEase={"linear"}
+        slidesToShow={2}
+        swipeToSlide={true}
+        focusOnSelect={true}
+      >
+        {returnValidColors(propertiesList, pk, albumImages).map(
+          (obj, index) => (
+            <img
+              className="w-max h-48 object-cover object-center"
+              src={obj}
+              key={index}
+              onClick={() => onClickIndex(index)}
+            />
+          )
+        )}
+      </Slider>
+    </div>
   );
 };
--- frontend/src/utils/hasColor.ts
+++ frontend/src/utils/hasColor.ts
@@ -1,7 +1,10 @@
 import { PropertiesType } from "@/redux/productProperties/type";
 import { albumImageType } from "@/@types/pages/Product";
 
-export const hasSizeColor = (colorList: PropertiesType[], productId: number) => {
+export const hasSizeColor = (
+  colorList: PropertiesType[],
+  productId: number
+) => {
   return colorList.find((obj) => obj.id === productId);
 };
 export const returnValidColor = (
@@ -11,4 +14,12 @@ export const returnValidColor = (
 ) => {
   const colorSize = hasSizeColor(colorList, productId);
   return colorSize ? albumImages[colorSize.color][0] : albumImages.black[0];
+};
+export const returnValidColors = (
+  colorList: PropertiesType[],
+  productId: number,
+  albumImages: albumImageType
+) => {
+  const colorSize = hasSizeColor(colorList, productId);
+  return colorSize ? albumImages[colorSize.color] : albumImages.black;
 };
